4f8e34350f1723b2bb471d189cdf7d77bf9671a5
Operrst <90959902+Operrst@users.noreply.github.com>
2022-06-08 17:10:11 +0200

    styling a bit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import TibiaMarket from './components';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <div>
+        <TibiaMarket/>
+        </div>
       </header>
     </div>
   );
