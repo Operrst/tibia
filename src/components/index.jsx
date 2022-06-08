49544771b9c31e8b6c64f7d25ac171af026f6971
Operrst <90959902+Operrst@users.noreply.github.com>
2022-06-08 18:53:42 +0200

    trying to make top player list

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -12,34 +12,63 @@ import "./styles.css";
              setTibia(res.data)
        })
    },[])
-return  (
-<body>
-     <div>
-        <div className="main-site-container">
-            <div className="main-header"></div>
-            <div className="main-menu"></div>
-            <div className="main-content">
-                <div className="border_1">
-                    <div className="border_2">
-                        <div className="BoxConent">
-                            <div className="TableContainer">
-                                <table className="table1">
-                                    <tbody>
-                                        <tr>
-                                            <td></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="main-themboxes"></div>
-        </div>
-     </div>
-</body>
-    )
+     return (
+         <form>
+             <table>
+                 <tbody>
+                   <tr>
+                     <td>Rank</td>
+                     <td>Name</td>
+                     <td>Vocation</td>
+                     <td>World</td>
+                     <td>Level</td>
+                     <td>Points</td>
+                   </tr>
+                   {characters.map((character)=>(
+                   <tr key={character.rank}>
+                       <td>{character.highscores.highscore_list.id}</td>
+                   </tr>
+                   ))}
+                 </tbody>
+             </table>
+         </form>
+     )
 }
 export default TibiaMarket;
 // highscores.highscore_list
+
+// return  (
+//     <form>
+//     <div>
+//        <div className="main-site-container">
+//            <div className="main-header"></div>
+//            <div className="main-menu"></div>
+//            <div className="main-content">
+//             <div className="highscores"> 
+//                <div className="border_1">
+//                    <div className="border_2">
+//                        <div className="BoxContent">
+//                            <div className="TableContainer">
+//                                <table className="table1">
+//                                    <tbody>
+//                                        <tr>
+//                                            <td>
+//                                                <div className="InnerTableContainer">
+//                                                   <div>"elo"</div>
+//                                                </div>
+//                                            </td>
+//                                        </tr>
+//                                    </tbody>
+//                                </table>
+//                            </div>
+//                        </div>
+//                    </div>
+//                </div>  
+//              </div> 
+//            </div>
+//            <div className="main-themboxes"></div>
+//        </div>
+//     </div>
+//     </form>
+//    )
+// }
