10e47b03d07bd9ae8ccbeb130aae413415375f89
Operrst <90959902+Operrst@users.noreply.github.com>
2022-06-15 13:33:39 +0200

    Character score

--- src/components/CharacterHighscore/index.jsx
+++ src/components/CharacterHighscore/index.jsx
@@ -5,17 +5,33 @@ import { useParams } from "react-router-dom";
 
 function Characters (){
     let { characterRank} = useParams();
-    const [char,setChar] = useState([])
+    const [char,setChar] = useState("")
         useEffect(()=>{
             axios.get(`https://api.tibiadata.com/v3/character/${characterRank}`).then((res)=>{
-                console.log(res)
-                setChar(res.data)
+                console.log(res.data)
+                setChar(res.data.characters.character)
             })
         },[])
         return (
-            <div>
-                <h1>hi</h1>
-            </div>
+           <table>
+           <tbody>
+                 
+               <tr><td>Name: {char.name}</td></tr>
+                <tr><td>Title: {char.title}</td></tr>
+                <tr><td>Sex: {char.sex}</td></tr>
+                <tr><td>Vocation: {char.vocation}</td></tr>
+                <tr><td>Level: {char.level}</td></tr>
+                <tr><td>Achievement Points: {char.achievement_points}</td></tr>
+                <tr><td>World: {char.world}</td></tr> 
+                <tr><td>Residence: {char.residence}</td></tr>
+                <tr><td>Married To: {char.married_to}</td></tr>
+                <tr><td>Guild Membership: {char.guild.name} {char.guild.rank}</td></tr>
+                <tr><td>Last Login: {char.last_login}</td></tr>
+                <tr><td>Comment: {char.comment}</td></tr>
+                <tr><td>Account Status: {char.account_status}</td></tr>
+                
+            </tbody>
+            </table>
         );
     
     
